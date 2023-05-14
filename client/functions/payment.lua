7847267cb34e7ea5ede3a73df96cbd11799d8fb8
Arius <70983185+Arius-Development@users.noreply.github.com>
2023-05-14 14:21:02 +0200

    added server callbacks

--- client/functions/payment.lua
+++ client/functions/payment.lua
@@ -1,8 +1,5 @@
 function confirmPayment()
     if not cart[1] then return end
-
-
-
     print(json.encode(cart, { indent = true }))
 
     local confirmation = lib.alertDialog({
@@ -12,9 +9,20 @@ function confirmPayment()
         cancel = true
     })
 
+    local cost = 1000
+
     if confirmation == "confirm" then
+        local hasMoney = lib.callback.await('ars_tuning:hasMoney', false, cost)
+
+        if not hasMoney then
+            lib.setVehicleProperties(cache.vehicle, currentVehProperties.old)
+            cart = {}
+            return
+        end
+
         cart = {}
         lib.setVehicleProperties(cache.vehicle, currentVehProperties.new)
+        TriggerServerEvent("ars_tuning:payMods", cost)
         return
     end
 
