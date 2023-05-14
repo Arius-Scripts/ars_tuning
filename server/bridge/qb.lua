59f741c8f786b67fdc5fdf12368f5226ecd4d5e0
Arius <70983185+Arius-Development@users.noreply.github.com>
2023-05-15 00:12:09 +0200

    added payment callback for qbcore

--- server/bridge/qb.lua
+++ server/bridge/qb.lua
@@ -2,3 +2,19 @@ local qb = GetResourceState('qb-core'):find("start")
 if not qb then return end
 
 local QBCore = exports['qb-core']:GetCoreObject()
+
+--- @param amount number
+lib.callback.register('ars_tuning:hasMoney', function(source, amount)
+    local xPlayer = QBCore.Functions.GetPlayer(source)
+
+    local money = xPlayer.Functions.GetMoney('cash')
+
+    return money >= amount
+end)
+
+--- @param amount number
+RegisterNetEvent("ars_tuning:payMods", function(amount)
+    local xPlayer = QBCore.Functions.GetPlayer(source)
+
+    xPlayer.Functions.RemoveMoney('cash', amount)
+end)
