21d82e2bae1032f2b3582e97b7e0813ddc6f6edc
Lexinor <alternonolimit@gmail.com>
2023-11-07 01:36:13 +0100

    cfg

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.WorkShops = {
     {
         job = "mechanic", -- false if you dont want to use the job
-        pos = vector3(-211.4554, -1323.0739, 30.8904),
+        pos = vector3(-208.1416, -1327.2565, 30.7101),
         blip = {
             enable = true,
             name   = "Bennys Tuning",
